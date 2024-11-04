f2befe4a0dfe6fae7154d937de9c70a05df2835e
BramBoos469 <106348725+BramBoos469@users.noreply.github.com>
2024-11-04 10:34:44 +0100

    .

--- 06 loops/03 Memory1D/Memory1D/Form1.cs
+++ 06 loops/03 Memory1D/Memory1D/Form1.cs
@@ -1,10 +1,8 @@
-
-
 namespace MenuDraw
 {
     public partial class Form1 : Form
     {
-        string[] names = ["1up", "star", "flower", "mushroom", "10coin", "20coin", "10coin", "20coin", "mushroom"];
+        string[] names = { "1up", "star", "flower", "mushroom", "10coin", "20coin", "10coin", "20coin", "mushroom" };
         const int w = 22;
         const int h = 32;
         const int rows = 3;
@@ -14,13 +12,13 @@ namespace MenuDraw
         Card[] cards;
         Dictionary<string, Rectangle> sprites = new Dictionary<string, Rectangle>()
         {
-            {"1up",new Rectangle(172,95,w,h) },
-            { "star",new Rectangle(140,95,w,h) },
-            { "flower",new Rectangle(108,95,w,h) },
-            { "mushroom",new Rectangle(76,95, w, h) },
-            { "10coin",new Rectangle(44,95, w, h) },
-            { "20coin" ,new Rectangle(12,95, w, h) },
-            { "cardback" ,new Rectangle(12,55, w, h) }
+            {"1up", new Rectangle(172, 95, w, h) },
+            { "star", new Rectangle(140, 95, w, h) },
+            { "flower", new Rectangle(108, 95, w, h) },
+            { "mushroom", new Rectangle(76, 95, w, h) },
+            { "10coin", new Rectangle(44, 95, w, h) },
+            { "20coin", new Rectangle(12, 95, w, h) },
+            { "cardback", new Rectangle(12, 55, w, h) }
         };
 
         public Form1()
@@ -34,20 +32,16 @@ namespace MenuDraw
 
         private void Form1_MouseClick(object? sender, MouseEventArgs e)
         {
-            //1) maak hier een for loop, die over cards loopt
+            // 1) Maak hier een for loop, die over cards loopt
             for (int i = 0; i < cards.Length; i++)
             {
-                //2) pak hier 1 card uit cards, die van [i]
-                ????
+                // 2) Pak hier 1 card uit cards, die van [i]
+                Card card = cards[i]; // Hier halen we de specifieke kaart op
 
-                //3) maak een if die kijkt of de muis X en Y in card.placement zit
-                // HINTS: 
-                // https://learn.microsoft.com/en-us/dotnet/api/system.drawing.rectangle.contains?view=net-8.0
-                // https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.mouseeventargs?view=windowsdesktop-8.0
-                
-                //HIER KOMT JE IF
+                // 3) Maak een if die kijkt of de muis X en Y in card.placement zit
+                if (card.placement.Contains(e.Location)) 
                 {
-                    //hier zeggen we dat de card omgedraait is
+                    
                     card.turned = true;
                 }
             }
@@ -90,11 +84,11 @@ namespace MenuDraw
             Graphics graphics = e.Graphics;
             graphics.Clear(Color.Black);
 
-            //4) maak hier een for loop, die over cards loopt
+            // 4) Maak hier een for loop, die over cards loopt
             for (int i = 0; i < cards.Length; i++)
             {
-                //5) geef hier de card die in [i] zit door op de ????
-                DrawCard(graphics, ???);
+                // 5) Geef hier de card die in [i] zit door op de ????
+                DrawCard(graphics, cards[i]); 
             }
         }
 
@@ -111,7 +105,7 @@ namespace MenuDraw
 
         internal void DoLogic(float frametime)
         {
-            //gebruiken we nu even niet
+            // Gebruiken we nu even niet
         }
     }
 }
