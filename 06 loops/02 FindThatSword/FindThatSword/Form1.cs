9e90bb2b780b74826cc4271949a91779b70911c7
BramBoos469 <106348725+BramBoos469@users.noreply.github.com>
2024-11-04 10:29:08 +0100

    .

--- 06 loops/02 FindThatSword/FindThatSword/Form1.cs
+++ 06 loops/02 FindThatSword/FindThatSword/Form1.cs
@@ -1,5 +1,3 @@
-
-
 namespace MenuDraw
 {
     public partial class Form1 : Form
@@ -13,26 +11,28 @@ namespace MenuDraw
 
             inventory = new Equipment[]
             {
-                new Equipment("The minotaur",EquipmentType.Axe),
-                new Equipment("The Grandfather",EquipmentType.Sword),
-                new Equipment("corpsemourn",EquipmentType.ChestArmor),
-                new Equipment("immortal kings pillar",EquipmentType.LegArmor)
+                new Equipment("The minotaur", EquipmentType.Axe),
+                new Equipment("The Grandfather", EquipmentType.Sword),
+                new Equipment("corpsemourn", EquipmentType.ChestArmor),
+                new Equipment("immortal kings pillar", EquipmentType.LegArmor)
             };
-
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
             e.Graphics.Clear(Color.Black);
-            //1) maak hier een for loop, die over inventory loopt
-            
-            {
 
-                //2) haal hier het item op [i] op, zie MenuDraw als je het even niet weet
-                Equipment item = ;
-                if ()//3) test hier of item.type GELIJK is aan EquipmentType.Sword
+            // 1) Maak hier een for loop die over inventory loopt
+            for (int i = 0; i < inventory.Length; i++)
+            {
+                // 2) Haal hier het item op [i] op
+                Equipment item = inventory[i];
+
+                // 3) Test hier of item.type gelijk is aan EquipmentType.Sword
+                if (item.type == EquipmentType.Sword)
                 {
+                    
                     e.Graphics.DrawImage(item.image, 0, 0);
                 }
             }
@@ -40,7 +40,7 @@ namespace MenuDraw
 
         internal void DoLogic(float frametime)
         {
-            //gebruiken we nu even niet
+            // Gebruiken we nu even niet
         }
     }
 }
