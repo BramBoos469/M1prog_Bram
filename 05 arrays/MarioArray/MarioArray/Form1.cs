8d9d65f058bf5465a7d0e1d56eb6c73bd9f86705
BramBoos469 <106348725+BramBoos469@users.noreply.github.com>
2024-11-04 09:52:41 +0100

    .

--- 05 arrays/MarioArray/MarioArray/Form1.cs
+++ 05 arrays/MarioArray/MarioArray/Form1.cs
@@ -4,18 +4,23 @@ namespace MarioArray
     {
         private readonly Image image;
         const int spriteSize = 16;
-		
-		//UITLEG: deze rectangle geeft aan welk stuk van het plaatje `sprites.png` getekend wordt, probeer het maar aan te passen!
+
+        //UITLEG: deze rectangle geeft aan welk stuk van het plaatje `sprites.png` getekend wordt, probeer het maar aan te passen!
         Rectangle frame = new Rectangle(1, 9, spriteSize, spriteSize);
-		
-		//UITLEG: elk punt is 1 plek waar we mario willen tekenen
+
+        //UITLEG: elk punt is 1 plek waar we mario willen tekenen
         Point mario1 = new Point(50, 50);
-		
-		
-		//1) vervang de ??? door het type wat daar nodig is
+
+
+        //1) vervang de ??? door het type wat daar nodig is
         // > hint: kijk wat er in hoort te zitten, dat kan je zien!
-        ??? otherMarios = new ???[] {
-            new Point(100,100)
+        Point[] otherMarios = new Point[] {
+            new Point(100, 100),
+            new Point(150, 120),
+            new Point(200, 140),
+            new Point(250, 160),
+            new Point(300, 180)
+
 			//2) zet hier nog 4 andere marios neer
 			
         };
